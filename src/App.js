1c3131bbb59e1c7715dcacf97fe766be4cc65cc2
Egor Zhuk <royalecool460@gmail.com>
2022-11-29 11:51:22 +0300

    fix warn

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
 
   async function fetchData() {
     try {
-      const responce = await axios
+      await axios
         .get(
           `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c&units=metric`
         )
